693b7e8f6aae4ed057b3ae28a79242b6d06ca277
Ryan Omizo <49911257+rotemple@users.noreply.github.com>
2024-11-01 12:39:59 -0400

    Update arliai_st_chat.py

--- arliai_st_chat.py
+++ arliai_st_chat.py
@@ -86,8 +86,8 @@ container = stylable_container(key="red_button",
 
                                """,
     )
-col1, col2 = st.columns(2)
-with col2:
+col1, col2 = st.columns(2,vertical_alignment='bottom')
+with col1:
     
     st.markdown('<h1 class="font">Llama 3.1 AI Paper/Panel Topic Generator</h1><h2>Built on <a href="https://www.arliai.com/">ARLI AI</a></h2>', unsafe_allow_html=True)
    
@@ -98,7 +98,7 @@ with col2:
 
     prompt = st.text_input("Prompt:", value="Generate cutting-edge AI paper ideas for a conference presentation . . .")
 
-    with col1:
+    with col2:
         temp = st.number_input('temperature',value=0.7)
         p = st.number_input('top p',value=.9)
         k = st.number_input('top k', value=40)
